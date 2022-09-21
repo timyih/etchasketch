4b110c4b73e0667b4764c5d1a68c511f5b2d26fc
Tim Yih <timmyyih@gmail.com>
2022-09-21 00:23:32 -0400

    Add javascript to button for resetting grid

--- javascript.js
+++ javascript.js
@@ -11,18 +11,36 @@ function createGrid(gridSize) {
         }
         container.appendChild(row);
     }
+}
 
+createGrid(30);
 
+var cells = document.querySelectorAll('div.cell');
+cells.forEach(cell => cell.addEventListener('mouseover', function () { cell.classList.add('colored') }));
 
-    const grid = document.createElement('div');
-    container.appendChild(grid);
+function reset() {
+    var size = prompt("Enter a number for the size of grid", 16);
 
-    grid.setAttribute('style', `display: grid; grid-template-columns: repeat(${gridSize}, 50px); grid-template-rows: repeat(${gridSize}, 50px); grid-gap: 5px;`);
-
-    for (let i = 0; i < gridSize*gridSize; i++) {
-        var div = document.createElement('div');
-        grid.appendChild(div);
+    if (!isNaN(size) && size <= 100) {
+        const container = document.querySelector('div.container');
+        removeAllChildren(container);
+        createGrid(size);
     }
 }
 
-createGrid(16);
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+// const resetButton = document.querySelector('button.reset');
+// resetButton.addEventListener('onclick', function () {
+//     var size = prompt("Enter a number for the size of grid", 16);
+//     alert(size);
+
+//     if (!isNaN(size) && size <= 100) {
+//         const rows = document.querySelectorAll('row');
+//         rows.forEach(row => row.remove());
+//         createGrid(size);
+//     }
+// });
